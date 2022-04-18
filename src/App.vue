2711a418b52b9270a2d96eb799a228a72d512697
denkoksz <denkoksz@gmail.com>
2022-04-18 18:36:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+      <template v-slot:links>
+        <a href="#">Sign up now!</a>
+        <a href="#">More info</a>
+      </template>
+    </Modal>
+  </teleport>
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>My second modal</h1>
+      <p>paragraph of the second modal</p>
+      <template v-slot:links>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+      </template>
+    </Modal>
+  </teleport>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click.ctrl="toggleModalTwo">open modal (ctrl)</button>
+<!--  <input type="text" ref="name">-->
+<!--  <button @click="handleClick">click me</button>-->
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue App :)',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale'}">
+<!--      <h1>{{ header }}</h1>-->
+<!--      <p>{{ text }}</p>-->
+      <slot>Default content</slot> <!-- csak akkor jelenik meg ez, ha ures a Modal template a root componentben-->
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close') //ilyen neven fogadja a root component propertykent
+    }
+  }
+}
+</script>
+
+<style>
+/*<style scoped>*/ /* same as -> .modal h1 , this will override the root */
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+  }
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
+</style>
